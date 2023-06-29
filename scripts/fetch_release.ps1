2bcad7bc4116741ca70a100de85f130d16104c4e
kajman <kajmaj87@gmail.com>
2023-06-29 16:43:38 +0200

    Added lazy fetching for windows script

--- scripts/fetch_release.ps1
+++ scripts/fetch_release.ps1
@@ -3,6 +3,7 @@ $REPO = "kajmaj87/sb3"
 $TOKEN = "REPLACE_WITH_TOKEN"
 $ARTIFACT_NAME = "sb3-x86_64-pc-windows-gnu"
 $UNPACK_DIR = "C:\temp\sb3"
+$RUN_ID_FILE = Join-Path $UNPACK_DIR "last_run_id.txt"
 
 # Make sure destination directory exists
 if (!(Test-Path -Path $UNPACK_DIR )) {
@@ -21,18 +22,31 @@ $RUN_ID = $response.workflow_runs | Where-Object { $_.conclusion -eq "success" }
 
 Write-Host "Run ID: $RUN_ID"
 
-# Get artifact URL
-$response = Invoke-RestMethod -Uri "https://api.github.com/repos/$REPO/actions/runs/$RUN_ID/artifacts" -Headers $headers -Method Get
-$ARTIFACT_URL = $response.artifacts | Where-Object { $_.name -eq "$ARTIFACT_NAME" } | ForEach-Object { $_.archive_download_url }
+# Get the last saved run ID
+$LAST_RUN_ID = if (Test-Path -Path $RUN_ID_FILE) {
+    Get-Content -Path $RUN_ID_FILE
+} else {
+    $null
+}
 
-Write-Host "Artifact URL: $ARTIFACT_URL"
+# Download and unpack the artifact if needed
+if ($RUN_ID -ne $LAST_RUN_ID) {
+    # Get artifact URL
+    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/$REPO/actions/runs/$RUN_ID/artifacts" -Headers $headers -Method Get
+    $ARTIFACT_URL = $response.artifacts | Where-Object { $_.name -eq "$ARTIFACT_NAME" } | ForEach-Object { $_.archive_download_url }
 
-# Download artifact
-$ARTIFACT_FILE = Join-Path $UNPACK_DIR "artifact.zip"
-Invoke-WebRequest -Uri $ARTIFACT_URL -Headers $headers -OutFile $ARTIFACT_FILE
+    Write-Host "Artifact URL: $ARTIFACT_URL"
 
-# Unzip the artifact
-Expand-Archive -Path $ARTIFACT_FILE -DestinationPath $UNPACK_DIR -Force
+    # Download artifact
+    $ARTIFACT_FILE = Join-Path $UNPACK_DIR "artifact.zip"
+    Invoke-WebRequest -Uri $ARTIFACT_URL -Headers $headers -OutFile $ARTIFACT_FILE
+
+    # Unzip the artifact
+    Expand-Archive -Path $ARTIFACT_FILE -DestinationPath $UNPACK_DIR -Force
+
+    # Save the run ID
+    Set-Content -Path $RUN_ID_FILE -Value $RUN_ID
+}
 
 # Run the binary
 $executable = Join-Path $UNPACK_DIR "sb3.exe"
