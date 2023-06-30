ab410aaee8d21f9c4fc2bab52a68d510c4949d69
kajman <kajmaj87@gmail.com>
2023-06-30 09:44:38 +0200

    Fixed the branch and commit infos

--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,5 @@
-use chrono::Utc;
+use chrono::{DateTime, Utc};
+use chrono_tz::Europe::Warsaw;
 use serde::Serialize;
 use serde_json::to_writer_pretty;
 use std::env;
@@ -14,31 +15,40 @@ struct BuildInfo {
 }
 
 fn main() {
-    let timestamp = Utc::now();
+    let now: DateTime<Utc> = Utc::now();
+    let warsaw_now = now.with_timezone(&Warsaw);
+    let formatted_now = warsaw_now.format("%Y-%m-%d %H:%M:%S").to_string();
 
-    let output = Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output()
-        .expect("Failed to execute git command");
+    // Try to get commit hash from GITHUB_SHA environment variable first, then fallback to git command
+    let git_hash = env::var("GITHUB_SHA").unwrap_or_else(|_| {
+        let output = Command::new("git")
+            .args(["rev-parse", "HEAD"])
+            .output()
+            .unwrap();
+        String::from_utf8(output.stdout).unwrap()
+    });
 
-    let commit_hash = String::from_utf8(output.stdout).unwrap().trim().to_string();
+    // Cut the SHA down to 7 characters.
+    let git_sha_short = &git_hash[0..7];
 
-    let output = Command::new("git")
-        .args(["rev-parse", "--abbrev-ref", "HEAD"])
-        .output()
-        .expect("Failed to execute git command");
+    // Try to get branch name from GITHUB_HEAD_REF environment variable first, then fallback to git command
+    let git_branch = env::var("GITHUB_HEAD_REF").unwrap_or_else(|_| {
+        let output = Command::new("git")
+            .args(["rev-parse", "--abbrev-ref", "HEAD"])
+            .output()
+            .unwrap();
+        String::from_utf8(output.stdout).unwrap().trim().to_string()
+    });
 
-    let branch_name = String::from_utf8(output.stdout).unwrap().trim().to_string();
+    let version = env!("CARGO_PKG_VERSION").to_string();
 
-    let version = env::var("CARGO_PKG_VERSION").unwrap();
-
-    let info = BuildInfo {
-        timestamp: timestamp.format("%Y-%m-%d %H:%M:%S").to_string(),
+    let build_info = BuildInfo {
+        timestamp: formatted_now,
+        commit_hash: git_sha_short.to_string(),
+        branch_name: git_branch,
         version,
-        commit_hash,
-        branch_name,
     };
 
     let file = File::create("build_info.json").expect("Failed to create file");
-    to_writer_pretty(file, &info).expect("Failed to write to file");
+    to_writer_pretty(file, &build_info).expect("Failed to write to file");
 }
