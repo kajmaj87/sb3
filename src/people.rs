2c6cc38a674ceef063b170c07564bd96786907b8
kajman <kajmaj87@gmail.com>
2023-07-15 10:31:42 +0200

    Calculating name collision probability

--- src/people.rs
+++ src/people.rs
@@ -16,7 +16,19 @@ impl Names {
         self.first_names = names.first_names;
         self.nicknames = names.nicknames;
         self.last_names = names.last_names;
+        let unique_names = self.first_names.len() as u64
+            * self.nicknames.len() as u64
+            * self.last_names.len() as u64;
+        info!(
+            "Loaded names. Possible unique combinations: {}",
+            unique_names
+        );
+        info!("Name collision probabilities for n people: 10: {:.3}%, 100: {:.3}%, 1000: {:.3}%, 10000: {:.3}%", collision_probability(10, unique_names), collision_probability(100, unique_names), collision_probability(1000, unique_names), collision_probability(10000, unique_names));
     }
+}
+
+fn collision_probability(samples: u64, unique_names: u64) -> f64 {
+    (1.0 - (-(samples as f64) * ((samples - 1) as f64) / (2.0 * unique_names as f64)).exp()) * 100.0
 }
 
 pub(crate) fn generate_name(names: &Res<Names>) -> String {
